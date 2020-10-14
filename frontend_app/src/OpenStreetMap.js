b81a6c0fbade6a2d6dd424675e03881fec06ad03
lsvagan <luka.svagan@gmail.com>
2020-10-14 13:36:49 +0200

    Create parent component MapAndInputContainer for holding the newPoiInfo state, and simplify logic of updateing state

--- frontend_app/src/OpenStreetMap.js
+++ frontend_app/src/OpenStreetMap.js
@@ -3,69 +3,8 @@ import { Map, TileLayer } from 'react-leaflet';
 import OpenStreetMapMarker from './OpenStreetMapMarker';
 import NewPoiMarker from './NewPoiMarker';
 import Search from './MapSearch';
-import NewPoiInput from './NewPoiInput';
 
   class MapComponent extends Component {
-    constructor() {
-      super();
-     
-      this.state = {
-      newPoi: {
-        lat: '',
-        lon: ''
-      }
-      };
-    }
-
-    //clear value of lat and lon, after adding poi action
-    clearLatLonInputs = () => {
-      let newPoi = this.state.newPoi;
-      newPoi.lat = '';
-      newPoi.lon = '';
-
-      this.setState({
-        newPoi: newPoi
-      })
-    }
-  
-    setLatLonOfNewPoi = (e) => {
-      
-      let updatedPoi;
-
-      // statement where we set lat lon using onChange event in form inputs
-      if(e.target.id){
-
-        if(e.target.id === "inputLat"){
-          updatedPoi = {
-            lat: e.target.value,
-            lon: this.state.newPoi.lon
-          }
-          this.setState({
-            newPoi : updatedPoi
-          })
-          return true;
-        }else{
-          updatedPoi = {
-            lat: this.state.newPoi.lat,
-            lon: e.target.value
-          }
-          this.setState({
-            newPoi : updatedPoi
-          })
-          return true;
-        }
-      }
-      let latValue = e.latlng.lat.toFixed(7);
-      let lonValue  = e.latlng.lng.toFixed(7);
-      //set lat lon using onClick event on map
-      this.setState({
-        newPoi : {
-          lat: latValue,
-          lon: lonValue
-        }
-      })
-      
-    }
     
     render() {
       
@@ -74,7 +13,7 @@ import NewPoiInput from './NewPoiInput';
           <Map className="leflet-map"
             center={[44.8110, 20.4625]}
             zoom={13}
-            onClick={this.setLatLonOfNewPoi}
+            onClick={this.props.setLatLonOfNewPoi}
             >
               <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -82,17 +21,10 @@ import NewPoiInput from './NewPoiInput';
               />
 
               <OpenStreetMapMarker pois = {this.props.pois} />
-              <NewPoiMarker newPoi = {this.state.newPoi} />
-              <Search setLatLonOfNewPoi = {this.setLatLonOfNewPoi} />
+              <NewPoiMarker newPoiInfo = {this.props.newPoiInfo} />
+              <Search setLatLonOfNewPoi = {this.props.setLatLonOfNewPoi} />
 
           </Map>
-
-          <NewPoiInput
-            setLatLonOfNewPoi={this.setLatLonOfNewPoi}
-            newPoiLatLon={ this.state.newPoi }
-            pushNewPoiInState = {this.props.pushNewPoiInState}
-            clearLatLonInputs = {this.clearLatLonInputs}
-          />
         </div>
       );
     }
